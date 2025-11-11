adb7975b2dd8293a871b15a9ddd69ee0717bd9d4
DH <8667333+Dennyg@users.noreply.github.com>
2025-11-11 16:54:16 +0200

    Add chat completion methods in DialClient
    Implemented chat completion methods using Dial and AsyncDial clients.

--- task/clients/client.py
+++ task/clients/client.py
@@ -10,10 +10,8 @@ class DialClient(BaseClient):
 
     def __init__(self, deployment_name: str):
         super().__init__(deployment_name)
-        #TODO:
-        # Documentation: https://pypi.org/project/aidial-client/ (here you can find how to create and use these clients)
-        # 1. Create Dial client
-        # 2. Create AsyncDial client
+        self.dial_client = Dial(api_key=self._api_key, base_url=DIAL_ENDPOINT)
+        self.async_dial_client = AsyncDial(api_key=self._api_key, base_url=DIAL_ENDPOINT)
 
     def get_completion(self, messages: list[Message]) -> Message:
         #TODO:
@@ -21,7 +19,19 @@ class DialClient(BaseClient):
         #    Hint: to unpack messages you can use the `to_dict()` method from Message object
         # 2. Get content from response, print it and return message with assistant role and content
         # 3. If choices are not present then raise Exception("No choices in response found")
-        raise NotImplementedError
+
+        completion = self.dial_client.chat.completions.create(
+            deployment_name=self._deployment_name,
+            stream=False,
+            messages=[m.to_dict() for m in messages]
+        )
+
+        if choices := completion.choices:
+            if message := choices[0].message:
+                print(message.content)
+                return Message(Role.AI, message.content)
+
+        raise Exception("No choices in response found")
 
     async def stream_completion(self, messages: list[Message]) -> Message:
         #TODO:
@@ -32,4 +42,22 @@ class DialClient(BaseClient):
         # 4. Print content chunk and collect it contents array
         # 5. Print empty row `print()` (it will represent the end of streaming and in console we will print input from a new line)
         # 6. Return Message with assistant role and message collected content
-        raise NotImplementedError
+
+        completion = await self.async_dial_client.chat.completions.create(
+            deployment_name=self._deployment_name,
+            stream=True,
+            messages=[m.to_dict() for m in messages]
+        )
+
+        content = []
+
+        async for chunk in completion:
+            if chunk.choices and len(chunk.choices) > 0:
+                delta = chunk.choices[0].delta
+                if delta and delta.content:
+                    print(delta.content, end='')
+                    content.append(delta.content) 
+
+        print()
+
+        return Message(Role.AI, ''.join(content))
